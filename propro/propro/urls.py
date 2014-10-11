1abc0a2dbbaf54e5621842a0d8f7c62c8c1d01be
wikibootup <wikibootup@gmail.com>
2014-10-11 11:49:25 +0900

    default UI for pyver

--- propro/propro/urls.py
+++ propro/propro/urls.py
@@ -1,8 +1,23 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from propro.views import Home
+from propro.views import Home, Lang
+from projects.views import PyVerView
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    url(r'^$', Home.as_view(), name='home'),    
+    url(r'^$', Home.as_view(), name='home'), 
+    url(r'^lang/', Lang.as_view(), name='lang'),
+    url(r'^pyver/', PyVerView.as_view(), name='pyver')
 )
+
+# for development
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT})
+    ]
