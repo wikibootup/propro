23fad0fd40751f25f375831c1fa1b3f43c188533
wikibootup <wikibootup@gmail.com>
2014-10-12 19:06:45 +0900

    docker text field added

--- propro/projects/views.py
+++ propro/projects/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.edit import FormView
-from projects.forms import PyVerForm
+from projects.forms import ProjectEnvForm
 from django.core.exceptions import ValidationError
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 
-class PyVerView(FormView):
-    template_name = "projects/pyver.html"
-    form_class = PyVerForm
+from projects.models import ProjectEnv
+from django.core.files import File
+
+class ProjectEnvView(FormView):
+    template_name = "projects/project_env.html"
+    form_class = ProjectEnvForm
+
 
     def form_valid(self, form):
-        select_pyver = self.request.POST["select_pyver"]
-        print self.request.POST.keys()
+        team_name = self.request.POST["team_name"]
+        project_name = self.request.POST["project_name"]
+        lang = self.request.POST["lang"]
+        ver = self.request.POST["ver"]
+        docker_text = ""
+        
+        if "python" in lang:
+            fileHandler = open(
+                    "media/docker/python_Dockerfile",
+                    'r'
+                    )
+            docker_text = fileHandler.read()
+            docker_text = docker_text.replace("<x.y>", ver[:3])
+            docker_text = docker_text.replace("<x.y.z>", ver)
 
-        return HttpResponse(select_pyver)
+        project_env = ProjectEnv.objects.create_project_env( 
+                team_name = team_name,
+                project_name = project_name,
+                properties = [lang, ver],
+                docker_text = docker_text,
+                )
+
+        return HttpResponse(docker_text)
+
 
