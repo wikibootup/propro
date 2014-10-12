149166741eacfea4acc8721f2700da0142ab98e9
wikibootup <wikibootup@gmail.com>
2014-10-12 19:06:21 +0900

    team, project, lang, ver textfield

--- propro/projects/models.py
+++ propro/projects/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from jsonfield import JSONField
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from django.db import IntegrityError
 
-# Create your models here.
+class ProjectEnvManager(models.Manager):
+    def create_project_env(self, team_name, project_name, properties, **kwargs):
+        self.validate_project_name(project_name)
+        project_env = self.model(
+                team_name = team_name,
+                project_name = project_name,
+                properties = properties,
+                )
+
+        if "docker_text" in kwargs:
+            project_env.docker_text = kwargs['docker_text']
+            
+        project_env.save(using = self._db)
+        return project_env
+
+    def validate_project_name(self, project_name):
+        try:
+            ProjectEnv.objects.get(project_name=project_name)
+        except:
+            return True
+        else:
+            raise ValidationError("The project name already exists")
+
+    def get_project_env(self, project_name):
+        try:
+            project_env = ProjectEnv.objects.get(project_name=project_name)
+        except ObjectDoesNotExist:
+            raise ObjectDoesNotExist(project_name + "Does not exist")
+        return project_env
+
+    def delete_project_env(self, project_name):
+        project_env = ProjectEnv.objects.get_project_env(project_env)
+        project_env.deactivate()
+        project_env.save(using = self._db)
+
+class ProjectEnv(models.Model):
+    team_name = models.CharField(default="none", max_length=20, unique=True)
+    project_name = models.CharField(default="none", max_length=20, unique=True)
+    properties = JSONField()
+    docker_text = models.TextField(default="none")
+    objects = ProjectEnvManager() # The default manager.
+
